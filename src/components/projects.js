878719044803424e63b4f79112d761f77d7ee678
Clint <maxeva02@gmail.com>
2021-05-22 17:22:42 -0700

    updating projects and added images

--- src/components/projects.js
+++ src/components/projects.js
@@ -14,7 +14,7 @@ class Projects extends Component {
       return(
         <div className="projects-grid">
           {/* Project 1 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#fff', height: '176px', marginTop:"5px", background: 'url(./images/javascript/calendar.jpg) center / cover'}}></CardTitle>
             <CardText>
               <h6><strong>Busy Parent Scheduler</strong></h6>
@@ -22,8 +22,8 @@ class Projects extends Component {
             </CardText>
             <CardActions border>
               <center>
-              <Button colored><a href="https://github.com/clintrizzo/BusyParentScheduler">GITHUB</a></Button>
-              <Button colored><a href="https://kjwallac.github.io/BusyParentScheduler/calendar.html" style={{color:"blue"}}>LIVE DEMO</a></Button>
+              <Button><a href="https://github.com/clintrizzo/BusyParentScheduler" style={{color:"black"}}>GITHUB</a></Button>
+              <Button><a href="https://kjwallac.github.io/BusyParentScheduler/calendar.html" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
@@ -32,16 +32,16 @@ class Projects extends Component {
           </Card>
 
           {/* Project 2 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', marginTop:"5px", background: 'url(./images/javascript/dayPlanner.jpg) center / cover'}} ></CardTitle>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', marginTop:"5px", background: 'url(./images/javascript/2d.jpg) center / cover'}} ></CardTitle>
             <CardText>
-            <h6><strong>Day Planner</strong></h6>
-            This app is mainly used to keep your daily work schedule organized. All work hours can be saved to the local storage of the user so that way your information is never lost. You can also use this calendar for reference showing you the past time, present, and future times all represented by a specific color. 1. past color: black 2. present color: light grey 3. future color: red Image of app:
+            <h6><strong>Brick Breaker</strong></h6>
+            Using the left and right directional pad you can move the bottom paddle to redirect the ball back to hitting the blocks. Goal of the game is to detroy all the paddles at the top to win the game. User has three lives if the ball hits below the bottom paddle the user loses a life. Future updates for this game is to add multpile levels to the game. Have the ball be launched when the user hits the enter button.
             </CardText>
             <CardActions border>
               <center>
-              <Button colored><a href="https://github.com/clintrizzo/day-planner">GITHUB</a></Button>
-              <Button colored><a href="https://clintrizzo.github.io/day-planner/">LIVE DEMO</a></Button>
+              <Button colored><a href="https://github.com/clintrizzo/2D-game" style={{color:"black"}}>GITHUB</a></Button>
+              <Button colored><a href="https://www.youtube.com/watch?v=P7NGOYOvt8Y" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
@@ -50,16 +50,16 @@ class Projects extends Component {
           </Card>
 
           {/* Project 3 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', marginTop:"5px", background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} ></CardTitle>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', marginTop:"5px", background: 'url(./images/javascript/weatherBoard.jpg) center / cover'}} ></CardTitle>
             <CardText>
-            <h6><strong>Day Planner</strong></h6>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            <h6><strong>Weather Board</strong></h6>
+            I built this project to display the weather for a city. Setting the current forecast to a card from boot strap and adding the five day projected weather on another card from bootstrap. In the current weather display it shows the current weather conditions with an appended image. I also appened the temp, humidity, and wind speed. In the search bar it will display the previous cities that you have searched.
             </CardText>
             <CardActions border>
               <center>
-              <Button colored><a href="https://github.com/clintrizzo/day-planner">GITHUB</a></Button>
-              <Button colored><a href="https://clintrizzo.github.io/day-planner/">LIVE DEMO</a></Button>
+              <Button colored><a href="https://github.com/clintrizzo/Weatherboard" style={{color:"black"}}>GITHUB</a></Button>
+              <Button colored><a href="https://clintrizzo.github.io/Weatherboard/" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
@@ -74,15 +74,15 @@ class Projects extends Component {
       return (
         <div className="projects-grid">
           {/* Project 1 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/mySQL/readMe.jpg) center / cover'}} >React Project #1</CardTitle>
             <CardText>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
             </CardText>
             <CardActions border>
               <center>
-              <Button colored>GitHub</Button>
-              <Button colored>Live Demo</Button>
+              <Button colored><a href="https://github.com/clintrizzo/Weatherboard" style={{color:"black"}}>GITHUB</a></Button>
+              <Button colored><a href="https://clintrizzo.github.io/Weatherboard/" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
@@ -91,15 +91,15 @@ class Projects extends Component {
           </Card>
 
           {/* Project 2 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
             <CardText>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
             </CardText>
             <CardActions border>
               <center>
-              <Button colored>GitHub</Button>
-              <Button colored>Live Demo</Button>
+              <Button colored><a href="https://github.com/clintrizzo/Weatherboard" style={{color:"black"}}>GITHUB</a></Button>
+              <Button colored><a href="https://clintrizzo.github.io/Weatherboard/" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
@@ -108,15 +108,15 @@ class Projects extends Component {
           </Card>
 
           {/* Project 3 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #3</CardTitle>
             <CardText>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
             </CardText>
             <CardActions border>
               <center>
-              <Button colored>GitHub</Button>
-              <Button colored>Live Demo</Button>
+              <Button colored><a href="https://github.com/clintrizzo/Weatherboard" style={{color:"black"}}>GITHUB</a></Button>
+              <Button colored><a href="https://clintrizzo.github.io/Weatherboard/" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
@@ -129,15 +129,15 @@ class Projects extends Component {
       return (
         <div className="projects-grid">
           {/* Project 1 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
             <CardText>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
             </CardText>
             <CardActions border>
               <center>
-              <Button colored>GitHub</Button>
-              <Button colored>Live Demo</Button>
+              <Button colored><a href="https://github.com/clintrizzo/Weatherboard" style={{color:"black"}}>GITHUB</a></Button>
+              <Button colored><a href="https://clintrizzo.github.io/Weatherboard/" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
@@ -146,15 +146,15 @@ class Projects extends Component {
           </Card>
 
           {/* Project 2 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
             <CardText>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
             </CardText>
             <CardActions border>
               <center>
-              <Button colored>GitHub</Button>
-              <Button colored>Live Demo</Button>
+              <Button colored><a href="https://github.com/clintrizzo/Weatherboard" style={{color:"black"}}>GITHUB</a></Button>
+              <Button colored><a href="https://clintrizzo.github.io/Weatherboard/" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
@@ -163,15 +163,15 @@ class Projects extends Component {
           </Card>
 
           {/* Project 3 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #3</CardTitle>
             <CardText>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
             </CardText>
             <CardActions border>
               <center>
-              <Button colored>GitHub</Button>
-              <Button colored>Live Demo</Button>
+              <Button colored><a href="https://github.com/clintrizzo/Weatherboard" style={{color:"black"}}>GITHUB</a></Button>
+              <Button colored><a href="https://clintrizzo.github.io/Weatherboard/" style={{color:"black"}}>LIVE DEMO</a></Button>
               </center>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
