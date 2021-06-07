7100fc02c4a75d9bb7c42ea58996ac5dc3f5de07
Clint <maxeva02@gmail.com>
2021-06-06 17:41:45 -0700

    added footer information

---
+++ src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+
+  const Footer = () => (
+    <div className="footer">
+      <p className="letters">@ Clint Rizzo</p>
+      <hr className="line"></hr>
+    </div>
+  );
+export default Footer;
