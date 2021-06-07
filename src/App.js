557d8ca4c13135253aeb210f99bcd1f68469ac47
Clint <maxeva02@gmail.com>
2021-06-06 17:41:05 -0700

    added footer to all pages

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
+import Footer from './components/footer/footer';
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Clinton Rizzo</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Clinton Rizzo</Link>}>
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/projects">Projects</Link>
@@ -26,6 +27,7 @@ class App extends Component {
         <Content>
             <div className="page-content" />
             <Main/>
+            <Footer/>
         </Content>
     </Layout>
 </div>
