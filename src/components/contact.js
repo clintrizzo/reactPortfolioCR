6f525acb5b173497b557a615b8c59e780ba05e33
Clint <maxeva02@gmail.com>
2021-06-08 10:06:33 -0700

    edited the message portion and added information about myself at the top of the page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import emailjs from 'emailjs-com';
 import '../App.css';
+import { List, ListItem, ListItemContent } from 'react-mdl';
 
 function Contact() {
     const sendEmail = (e) => {
@@ -14,44 +15,68 @@ function Contact() {
               });
               e.target.reset()
     }
-
     return (
-        
         <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="well well-sm">
-                        <form className="form-horizontal" onSubmit={sendEmail}>
-                            <fieldset>
-                                <legend className="text-left" style={{color:"white", paddingTop: "150px", paddingLeft: "15px"}}>Contact Us</legend>
-
-                                <div className="form-group">
-                                    <label className="col-md-3 control-label" for="name" style={{color:"white", marginTop:"15px"}}>Name</label>
-                                    <div className="col-md-9">
-                                        <input id="name" name="name" type="text" placeholder="Your name" class="form-control" />
-                                    </div>
+            <div>
+                <div>
+                    <div>
+                        <center>
+                        <div className="contact-list">
+                            <h1>Contact Me</h1>
+                            <center>
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '20px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        (951)288-2247
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '20px', color: 'black', fontFamily: 'Anton'}}>
+                                    <a href="https://github.com/clintrizzo" rel="noopener noreferrer" target="_blank" style={{color:'black'}}>
+                                        <i className="fa fa-github-square" aria-hidden="true" style={{color:'black'}}/>
+                                        github.com/clintrizzo
+                                    </a>
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '20px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+                                        clint.rizzo92@gmail.com
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                            </center>
+                        </div>
+                        </center>
+                        <hr></hr>
+                        <form onSubmit={sendEmail}>
+                            <legend className="text-left" style={{color:"black", paddingTop: "150px", paddingLeft: "15px"}}>Send me a message</legend>
+                            <div className="form-group">
+                                <label for="name" style={{color:"black", marginTop:"15px"}}>Name</label>
+                                <div>
+                                    <input id="name" name="name" type="text" placeholder="Your name" class="form-control" />
                                 </div>
-
-                                <div className="form-group">
-                                    <label className="col-md-3 control-label" for="email" style={{color:"white"}}>Your E-mail</label>
-                                    <div className="col-md-9">
-                                        <input id="email" name="email" type="text" placeholder="Your email" class="form-control" />
-                                    </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="email" style={{color:"black"}}>Your E-mail</label>
+                                <div>
+                                    <input id="email" name="email" type="text" placeholder="Your email" class="form-control" />
                                 </div>
-
-                                <div className="form-group">
-                                    <label className="col-md-12 control-label" for="message" style={{color: "white"}}>Your message</label>
-                                    <div className="col-md-9">
-                                        <textarea className="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
-                                    </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="message" style={{color: "black"}}>Your message</label>
+                                <div>
+                                    <textarea className="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="12"></textarea>
                                 </div>
-
-                                <div className="form-group">
-                                    <div className="col-md-12 text-center">
-                                        <button type="submit" className="button">Submit</button>
-                                    </div>
+                            </div>
+                            <div className="form-group">
+                                <div>
+                                    <button type="submit" className="button">Submit</button>
                                 </div>
-                            </fieldset>
+                                <br></br>
+                                <br></br>
+                            </div>
                         </form>
                     </div>
                 </div>
