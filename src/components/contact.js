7c5499e60a2956bc926e6b10f6aac289d4d30c52
Clint <maxeva02@gmail.com>
2021-06-08 22:05:57 -0700

    deleted listed items

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,6 @@
 import React from "react";
 import emailjs from 'emailjs-com';
 import '../App.css';
-import { List, ListItem, ListItemContent } from 'react-mdl';
 
 function Contact() {
     const sendEmail = (e) => {
@@ -20,36 +19,6 @@ function Contact() {
             <div>
                 <div>
                     <div>
-                        <center>
-                        <div className="contact-list">
-                            <h1>Contact Me</h1>
-                            <center>
-                            <List>
-                                <ListItem>
-                                    <ListItemContent style={{fontSize: '20px', fontFamily: 'Anton'}}>
-                                        <i className="fa fa-phone-square" aria-hidden="true"/>
-                                        (951)288-2247
-                                    </ListItemContent>
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemContent style={{fontSize: '20px', color: 'black', fontFamily: 'Anton'}}>
-                                    <a href="https://github.com/clintrizzo" rel="noopener noreferrer" target="_blank" style={{color:'black'}}>
-                                        <i className="fa fa-github-square" aria-hidden="true" style={{color:'black'}}/>
-                                        github.com/clintrizzo
-                                    </a>
-                                    </ListItemContent>
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemContent style={{fontSize: '20px', fontFamily: 'Anton'}}>
-                                        <i className="fa fa-envelope" aria-hidden="true"/>
-                                        clint.rizzo92@gmail.com
-                                    </ListItemContent>
-                                </ListItem>
-                            </List>
-                            </center>
-                        </div>
-                        </center>
-                        <hr></hr>
                         <form onSubmit={sendEmail}>
                             <legend className="text-left" style={{color:"black", paddingTop: "150px", paddingLeft: "15px"}}>Send me a message</legend>
                             <div className="form-group">
