674c1ec1d74a2c7a4789e766822313ceee1926a3
Clint <maxeva02@gmail.com>
2021-06-09 10:14:20 -0700

    added social icons for phone and email. fixed the sizing and color again

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-mdl';
 
-
 class Landing extends Component {
   render() {
     return(
@@ -9,35 +8,32 @@ class Landing extends Component {
         <Grid className="landing-grid">
             <div className="banner-text">
               <h1>Clinton Rizzo</h1>
+              <div clasName='connect'>
+                <i class="fa fa-phone" style={{color:'white', paddingBottom:'30px', paddingRight:'20px'}}> (951) 288-2247 </i><i class="fa fa-envelope" style={{color:'white', paddingBottom:'30px', paddingLeft:'20px'}}> clint.rizzo92@gmail.com</i>
+              </div>
               <hr style={{width:'50%'}}></hr>
               <h2 className='title'>Full Stack Web Developer</h2>
-            <hr/>
+              <hr/>
+              <p className='languages'>HTML/CSS | Bootstrap | JavaScript | MySQL | NodeJS | Express | MongoDB | React</p>
+                <div className="social-links">
+                {/* LinkedIn */}
+                  <a href="https://www.linkedin.com/in/clinton-rizzo-79b67b1b6/" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                  </a>
+                {/* Github */}
+                  <a href="https://github.com/clintrizzo" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-github-square" aria-hidden="true" />
+                  </a>
+                {/* Instagram */}
+                  <a href="https://www.instagram.com/clintrizzo/" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-instagram" aria-hidden="true" />
+                  </a>
+                {/* Youtube */}
+                  <a href="https://www.youtube.com/channel/UCpARYFoliOIr-UGpIT-4ibQ" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-youtube-square" aria-hidden="true" />
+                  </a>
 
-          <p className='languages'>HTML/CSS | Bootstrap | JavaScript | MySQL | NodeJS | Express | MongoDB | React</p>
-
-        <div className="social-links">
-
-          {/* LinkedIn */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-linkedin-square" aria-hidden="true" />
-          </a>
-
-          {/* Github */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-github-square" aria-hidden="true" />
-          </a>
-
-          {/* Freecodecamp */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-free-code-camp" aria-hidden="true" />
-          </a>
-
-          {/* Youtube */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-youtube-square" aria-hidden="true" />
-          </a>
-
-        </div>
+                </div>
             </div>
         </Grid>
       </div>
