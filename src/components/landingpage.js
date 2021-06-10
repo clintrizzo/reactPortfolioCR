b5df9a67967207a745ce329851e80fe03e82a2bd
Clint <maxeva02@gmail.com>
2021-06-09 23:23:00 -0700

    updated landing page to be more organized

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -11,26 +11,25 @@ class Landing extends Component {
               <hr style={{width:'50%'}}></hr>
               <h2 className='title'>Full Stack Web Developer</h2>
               <hr/>
-              <p className='languages'>HTML/CSS | Bootstrap | JavaScript | MySQL | NodeJS | Express | MongoDB | React</p>
-              <hr style={{paddingBottom:'20px', width:'50%'}}/>
+              <br></br>
+              <br></br>
                 <div className="social-links">
-                {/* LinkedIn */}
-                  <a href="https://www.linkedin.com/in/clinton-rizzo-79b67b1b6/" rel="noopener noreferrer" target="_blank">
-                    <i className="fa fa-linkedin-square" aria-hidden="true" />
-                  </a>
-                {/* Github */}
+                  {/* Github */}
                   <a href="https://github.com/clintrizzo" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-github-square" aria-hidden="true" />
                   </a>
-                {/* Instagram */}
-                  <a href="https://www.instagram.com/clintrizzo/" rel="noopener noreferrer" target="_blank">
-                    <i className="fa fa-instagram" aria-hidden="true" />
-                  </a>
-                {/* Youtube */}
+                  {/* Youtube */}
                   <a href="https://www.youtube.com/channel/UCpARYFoliOIr-UGpIT-4ibQ" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-youtube-square" aria-hidden="true" />
                   </a>
-
+                  {/* LinkedIn */}
+                  <a href="https://www.linkedin.com/in/clinton-rizzo-79b67b1b6/" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                  </a>
+                  {/* Instagram */}
+                  <a href="https://www.instagram.com/clintrizzo/" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-instagram" aria-hidden="true" />
+                  </a>
                 </div>
             </div>
         </Grid>
