28c5f4148fae0a6fc8e4faf9c9881bd35b39d251
Clint <maxeva02@gmail.com>
2021-06-09 21:45:40 -0700

    inserted image

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,7 +16,7 @@ class Projects extends Component {
         <div className="projects-grid">
           {/* Project 1 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', marginTop:"5px", background: 'url(./images/javascript/calendar.jpg) center / cover'}}></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', marginTop:"5px", background: 'url(./images/javascript/calendar.jpg) center / cover'}}></CardTitle>
             <CardText>
               <h6><strong>Busy Parent Scheduler</strong></h6>
               We built this app for the intention of parents who have a busy life style that they could schedule their work week, personal life, and childrens schedule all in one place. For families that like to plan in advance the app allows you to select days weeks away and still be able to put in a "to do" item. The user will also have updated weather depending on the location they are at when they open the calendar.
@@ -34,7 +34,7 @@ class Projects extends Component {
           <br></br>
           {/* Project 2 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', marginTop:"5px", background: 'url(./images/javascript/2d.jpg) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', marginTop:"5px", background: 'url(./images/javascript/2d.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Brick Breaker</strong></h6>
             Using the left and right directional pad you can move the bottom paddle to redirect the ball back to hitting the blocks. Goal of the game is to detroy all the paddles at the top to win the game. User has three lives if the ball hits below the bottom paddle the user loses a life. Future updates for this game is to add multpile levels to the game. Have the ball be launched when the user hits the enter button.
@@ -52,7 +52,7 @@ class Projects extends Component {
           <br></br>
           {/* Project 3 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', marginTop:"5px", background: 'url(./images/javascript/weatherBoard.jpg) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', marginTop:"5px", background: 'url(./images/javascript/weatherBoard.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Weather Board</strong></h6>
             I built this project to display the weather for a city. Setting the current forecast to a card from boot strap and adding the five day projected weather on another card from bootstrap. In the current weather display it shows the current weather conditions with an appended image. I also appened the temp, humidity, and wind speed. In the search bar it will display the previous cities that you have searched.
@@ -80,7 +80,7 @@ class Projects extends Component {
         <div className="projects-grid">
           {/* Project 1 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/mySQL/readMe.jpg) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/mySQL/readMe.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>ReadMe Generator</strong></h6>
               Application was built to create a readme file. User can start program by running the terminal with "node server.js" then they will be prompted to answer a few questions. Once questions are answered a readme file will be created
@@ -98,7 +98,7 @@ class Projects extends Component {
           <br></br>
           {/* Project 2 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/mySQL/employeeTracker.jpg) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/mySQL/employeeTracker.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Employee Tracker</strong></h6>
               Application was built so user can update an employee field, an active department, and include salaries. To run this program user needs to go into the terminal and run "node server.js". 
@@ -116,7 +116,7 @@ class Projects extends Component {
         <br></br>
           {/* Project 3 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/mySQL/teamProfile.jpg) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/mySQL/teamProfile.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Team Profile</strong></h6>
             This application was built using node.js, user can start the program with "node index.js". The user will then be prompted to answer questions based on an employee that is working at their place of business.
@@ -142,7 +142,7 @@ class Projects extends Component {
         <div className="projects-grid">
           {/* Project 1 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/react/userDirectory.jpg) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/react/userDirectory.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>User Directory</strong></h6>
             This is a employee directory created with React. The application's UI is broken up into components, manages component state, and responds to user events.            </CardText>
@@ -159,7 +159,7 @@ class Projects extends Component {
         <br></br>
           {/* Project 2 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/react/googleSearch.jpg) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/react/googleSearch.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Google Book Search</strong></h6>
             This app construction used React components, helper/util functions, and utilized React lifecycle methods to query and display books based on user searches.
@@ -178,7 +178,7 @@ class Projects extends Component {
           <br></br>
           {/* Project 3 */}
           <Card shadow={10} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/react/joyHunter.jpg) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/react/joyHunter.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Joy Hunter</strong></h6>
             Using react and mySQL we created this website to help the user find different paintball and airsoft games. For this project I did the frontend work for the pages.
@@ -207,15 +207,15 @@ class Projects extends Component {
   render() {
     return(
       <div>
-        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple style={{paddingLeft:'20px', marginLeft:'-15px', marginTop:'5px'}}>
           <Tab>Javascript</Tab>
-          <Tab>mySQL/NodeJS</Tab>
+          <Tab>NodeJS</Tab>
           <Tab>React</Tab>
         </Tabs>
 
 
           <Grid>
-            <Cell col={12}>
+            <Cell col={8}>
               <div className="content">{this.toggleCategories()}</div>
             </Cell>
           </Grid>
