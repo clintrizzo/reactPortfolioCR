bb581f26de7c31fa0b2659a0ac6ccf8b77db7b94
Clint <maxeva02@gmail.com>
2021-06-10 22:12:27 -0700

    removed hr tag

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,6 @@ import "./footer.css";
   const Footer = () => (
     <div className="footer">
       <a href="https://github.com/clintrizzo" rel="noopener noreferrer" target="_blank" className='letters'>@ Clinton Rizzo</a>
-      <hr className="line"></hr>
     </div>
   );
 export default Footer;
