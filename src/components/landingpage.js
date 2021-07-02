c8cdc2219e221c96ee95700b1e92bd6dac0da278
Clint <maxeva02@gmail.com>
2021-07-02 09:27:38 -0700

    created div tags for each social link to be hidden text till user hovers over the link

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -18,22 +18,27 @@ class Landing extends Component {
                   {/* Github */}
                   <a href="https://github.com/clintrizzo" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-github-square" aria-hidden="true" />
+                    <div className='hiddenText0'>Github</div>
                   </a>
                   {/* Youtube */}
                   <a href="https://www.youtube.com/channel/UCpARYFoliOIr-UGpIT-4ibQ" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-youtube-square" aria-hidden="true" />
+                    <div className='hiddenText1'>Youtube</div>
                   </a>
                   {/* LinkedIn */}
                   <a href="https://www.linkedin.com/in/clinton-rizzo-79b67b1b6/" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-linkedin-square" aria-hidden="true" />
+                    <div className='hiddenText2'>Linkedin</div>
                   </a>
                   {/* Instagram */}
                   <a href="https://www.instagram.com/clintrizzo/" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-instagram" aria-hidden="true" />
+                    <div className='hiddenText3'>Instagram</div>
                   </a>
                   {/*resume*/}
-                  <a href="https://www.instagram.com/clintrizzo/" rel="noopener noreferrer" target="_blank">
+                  <a href="https://docs.google.com/document/d/1qoSVfiUBKsaPGNi_9KM9QEsKNl-7HqC2UACmeYMM2XQ/edit?usp=sharing" rel="noopener noreferrer" target="_blank">
                     <i className="fa fa-folder-open" aria-hidden="true" />
+                    <div className='hiddenText4'>Resume</div>
                   </a>
                 </div>
             </div>
