fa86487202fecb16bbf0ee7421ac57423979468f
Clint <maxeva02@gmail.com>
2021-07-06 20:30:01 -0700

    fixed the column issues

--- src/components/experience.js
+++ src/components/experience.js
@@ -5,11 +5,11 @@ class Experience extends Component {
   render() {
     return(
       <Grid>
-        <Cell col={8}>
+        <Cell col={4}>
           <p className="startTime">{this.props.startYear} - {this.props.endYear}</p>
         </Cell>
         <Cell col={8}>
-          <h4 className='jobName' style={{marginTop:'0px', fontSize:'25px'}}>{this.props.jobName}</h4>
+          <h4 className='jobName' style={{marginTop:'0px', fontSize:'30px'}}>{this.props.jobName}</h4>
           <p className="jobDescription">{this.props.jobDescription}</p>
         </Cell>
       </Grid>
