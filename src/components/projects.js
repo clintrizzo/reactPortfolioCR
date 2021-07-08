4a64cd7cd5ea59ca4b6670a11172ee5b316f73de
Clint <maxeva02@gmail.com>
2021-07-08 13:59:01 -0700

    added hr tags under all titles for a cleaner look

--- src/components/projects.js
+++ src/components/projects.js
@@ -19,7 +19,8 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', marginTop:"5px", background: 'url(./images/javascript/calendar.jpg) center / cover'}}></CardTitle>
             <CardText>
               <h6><strong>Busy Parent Scheduler</strong></h6>
-              We built this app for the intention of parents who have a busy life style that they could schedule their work week, personal life, and childrens schedule all in one place. For families that like to plan in advance the app allows you to select days weeks away and still be able to put in a "to do" item. The user will also have updated weather depending on the location they are at when they open the calendar.
+              <hr style={{width:'100%'}}></hr>
+              <p className='description'>We built this app for the intention of parents who have a busy life style that they could schedule their work week, personal life, and childrens schedule all in one place. For families that like to plan in advance the app allows you to select days weeks away and still be able to put in a "to do" item. The user will also have updated weather depending on the location they are at when they open the calendar.</p>
             </CardText>
             <CardActions border>
               <center>
@@ -37,7 +38,8 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', marginTop:"5px", background: 'url(./images/javascript/2d.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Brick Breaker</strong></h6>
-            Using the left and right directional pad you can move the bottom paddle to redirect the ball back to hitting the blocks. Goal of the game is to detroy all the paddles at the top to win the game. User has three lives if the ball hits below the bottom paddle the user loses a life. Future updates for this game is to add multpile levels to the game. Have the ball be launched when the user hits the enter button.
+            <hr style={{width:'100%'}}></hr>
+            <p className='description'>Using the left and right directional pad you can move the bottom paddle to redirect the ball back to hitting the blocks. Goal of the game is to detroy all the paddles at the top to win the game. User has three lives if the ball hits below the bottom paddle the user loses a life. Future updates for this game is to add multpile levels to the game. Have the ball be launched when the user hits the enter button.</p>
             </CardText>
             <CardActions border>
               <center>
@@ -55,7 +57,8 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', marginTop:"5px", background: 'url(./images/javascript/weatherBoard.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Weather Board</strong></h6>
-            I built this project to display the weather for a city. Setting the current forecast to a card from boot strap and adding the five day projected weather on another card from bootstrap. In the current weather display it shows the current weather conditions with an appended image. I also appened the temp, humidity, and wind speed. In the search bar it will display the previous cities that you have searched.
+            <hr style={{width:'100%'}}></hr>
+            <p className='description'>I built this project to display the weather for a city. Setting the current forecast to a card from boot strap and adding the five day projected weather on another card from bootstrap. In the current weather display it shows the current weather conditions with an appended image. I also appened the temp, humidity, and wind speed. In the search bar it will display the previous cities that you have searched.</p>
             </CardText>
             <CardActions border>
               <center>
@@ -80,7 +83,8 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/mySQL/readMe.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>ReadMe Generator</strong></h6>
-              Application was built to create a readme file. User can start program by running the terminal with "node server.js" then they will be prompted to answer a few questions. Once questions are answered a readme file will be created
+            <hr style={{width:'100%'}}></hr>
+            <p className='description'>Application was built to create a readme file. User can start program by running the terminal with "node server.js" then they will be prompted to answer a few questions. Once questions are answered a readme file will be created</p>
             </CardText>
             <CardActions border>
               <center>
@@ -98,7 +102,8 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/mySQL/employeeTracker.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Employee Tracker</strong></h6>
-              Application was built so user can update an employee field, an active department, and include salaries. To run this program user needs to go into the terminal and run "node server.js". 
+            <hr style={{width:'100%'}}></hr>
+            <p className='description'>Application was built so user can update an employee field, an active department, and include salaries. To run this program user needs to go into the terminal and run "node server.js".</p>
             </CardText>
             <CardActions border>
               <center>
@@ -116,7 +121,8 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/mySQL/teamProfile.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Team Profile</strong></h6>
-            This application was built using node.js, user can start the program with "node index.js". The user will then be prompted to answer questions based on an employee that is working at their place of business.
+            <hr style={{width:'100%'}}></hr>
+            <p className='description'>This application was built using node.js, user can start the program with "node index.js". The user will then be prompted to answer questions based on an employee that is working at their place of business.</p>
             </CardText>
             <CardActions border>
               <center>
@@ -139,7 +145,9 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/react/userDirectory.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>User Directory</strong></h6>
-            This is a employee directory created with React. The application's UI is broken up into components, manages component state, and responds to user events.            </CardText>
+            <hr style={{width:'100%'}}></hr>
+            <p className='description'>This is a employee directory created with React. The application's UI is broken up into components, manages component state, and responds to user events.</p>            
+            </CardText>
             <CardActions border>
               <center>
               <Button colored><a href="https://github.com/clintrizzo/user-directory2" rel="noopener noreferrer" target="_blank" style={{color:"black"}}>GITHUB</a></Button>
@@ -156,7 +164,8 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/react/googleSearch.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Google Book Search</strong></h6>
-            This app construction used React components, helper/util functions, and utilized React lifecycle methods to query and display books based on user searches.
+            <hr style={{width:'100%'}}></hr>
+            <p className='description'>This app construction used React components, helper/util functions, and utilized React lifecycle methods to query and display books based on user searches.</p>
 
             </CardText>
             <CardActions border>
@@ -175,7 +184,8 @@ class Projects extends Component {
             <CardTitle style={{color: '#fff', height: '180px', background: 'url(./images/react/joyHunter.jpg) center / cover'}} ></CardTitle>
             <CardText>
             <h6><strong>Joy Hunter</strong></h6>
-            Using react and mySQL we created this website to help the user find different paintball and airsoft games. For this project I did the frontend work for the pages.
+            <hr style={{width:'100%'}}></hr>
+            <p className='description'>Using react and mySQL we created this website to help the user find different paintball and airsoft games. For this project I did the frontend work for the pages.</p>
             </CardText>
             <CardActions border>
               <center>
