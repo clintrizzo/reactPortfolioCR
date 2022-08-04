2ca3373e81e9b4104185d9759892701ef6583e47
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2022-08-03 20:35:56 -0700

    test
    test

--- src/components/projectsPage/projectDetails/brickBreacker.js
+++ src/components/projectsPage/projectDetails/brickBreacker.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import CardOption from '../button'
+import CardOption from '../projectDetails'
 export default function App() {
   const [toggle, setToggle] = useState(false);
   // const initialText = 'Click';
@@ -26,7 +26,7 @@ export default function App() {
           }}
         >
           <center>
-            <h3 style={{cursor:'pointer'}} className='busyParentScheduler' ><CardOption></CardOption></h3>
+            <h3 style={{cursor:'pointer'}} className='dropdownInfo' ><CardOption></CardOption></h3>
           </center>
         </div>
         <div
@@ -37,15 +37,15 @@ export default function App() {
         >
         <Card.Text>
           <br></br>
-          <p className='description'>Using the left and right directional pad you can move the bottom paddle to redirect the ball back to hitting the blocks. Goal of the game is to detroy all the paddles at the top to win the game. User has three lives if the ball hits below the bottom paddle the user loses a life. </p>   
+          <p className='description' id="projectText">Using the left and right directional pad you can move the bottom paddle to redirect the ball back to hitting the blocks. Goal of the game is to detroy all the paddles at the top to win the game. User has three lives if the ball hits below the bottom paddle the user loses a life. </p>   
         </Card.Text>
           <hr style={{width:'100%', backgroundColor:'red'}}></hr>
         <Card.Footer style={{paddingBottom: '25px'}}>
           <center>
-          <Button variant="primary" size="lg" active>
+          <Button variant="primary" size="lg" active className="links">
           <a href="https://www.youtube.com/watch?v=P7NGOYOvt8Y" rel="noopener noreferrer" target="_blank" style={{color: 'white'}}>Deployment</a>
           </Button>{' '}
-          <Button variant="secondary" size="lg" active>
+          <Button variant="secondary" size="lg" active className="links">
           <a href="https://github.com/clintrizzo/2D-game" rel="noopener noreferrer" target="_blank" style={{color: 'white'}} >GITHUB</a>                
           </Button>    
           </center>    
--- src/components/projectsPage/projectDetails/weatherDashboard.js
+++ src/components/projectsPage/projectDetails/weatherDashboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import CardOption from '../button'
+import CardOption from '../projectDetails'
 export default function App() {
   const [toggle, setToggle] = useState(false);
   // const initialText = 'Click';
@@ -26,7 +26,7 @@ export default function App() {
           }}
         >
           <center>
-            <h3 style={{cursor:'pointer'}} className='busyParentScheduler' ><CardOption></CardOption></h3>
+            <h3 style={{cursor:'pointer'}} className='dropdownInfo' ><CardOption></CardOption></h3>
           </center>
         </div>
         <div
@@ -37,15 +37,15 @@ export default function App() {
         >
         <Card.Text>
           <br></br>
-          <p className='description'>I built this project to display the weather for a city. Setting the current forecast to a card from boot strap and adding the five day projected weather on another card. In the current weather display it shows the current weather conditions, temp, humidity, and wind speed. In the search bar it will display the previous cities that you have searched.</p>   
+          <p className='description' id="projectText">I built this project to display the weather for a city. Setting the current forecast to a card from boot strap and adding the five day projected weather on another card. In the current weather display it shows the current weather conditions, temp, humidity, and wind speed.</p>   
         </Card.Text>
           <hr style={{width:'100%', backgroundColor:'red'}}></hr>
         <Card.Footer style={{paddingBottom: '25px'}}>
           <center>
-          <Button variant="primary" size="lg" active>
+          <Button variant="primary" size="lg" active className="links">
           <a href="https://clintrizzo.github.io/Weatherboard/" rel="noopener noreferrer" target="_blank" style={{color: 'white'}}>Deployment</a>
           </Button>{' '}
-          <Button variant="secondary" size="lg" active>
+          <Button variant="secondary" size="lg" active className="links">
           <a href="https://github.com/clintrizzo/Weatherboard" rel="noopener noreferrer" target="_blank" style={{color: 'white'}} >GITHUB</a>                
           </Button>    
           </center>    
